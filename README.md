12f224897bf2e054f84a90c212afe7755db8e1fc
Glitch (curious-octocat) <none>
2018-09-24 16:37:20 +0000

    💏🐦 Updated with Glitch

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# find-gh-pages
-Rediscover which GitHub repos you've published to gh-pages.
+Discover your gh-pages
+=================
+
+Rediscover which GitHub repositories you've published on gh-pages.
+
+- A [Glitch](https://glitch.com/about)  app!
+- Check it out: [https://curious-octocat.glitch.me/](https://curious-octocat.glitch.me/)
---
+++ public/client.js
@@ -0,0 +1,62 @@
+const pages = document.querySelector('#pages');
+
+const searchForm = document.querySelector('#search');
+searchForm.addEventListener('submit', function (e) {
+  e.preventDefault();
+  const username = this.user.value;
+  fetch(`/pages/${username}`)
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(projects) {
+      pages.innerText = '';
+      projects.forEach(function(project) {new Project(project);});
+    });
+  
+  this.user.value = '';
+});
+
+class Project {
+  constructor ({name, url, description}) {
+    this.name = name;
+    this.url = url;
+    this.description = description;
+    
+    this.ele = document.createElement('li');
+    
+    // TODO - use a <template>
+    this.addToDom();
+  }
+
+  addToDom () {
+    this.createTitle();
+    this.createDescription();
+    this.createLink();
+    
+    pages.appendChild(this.ele);
+  }
+  
+  createTitle () {
+    const title = document.createElement('h1');
+    title.innerText = this.name;
+    
+    this.ele.appendChild(title);
+  }
+  
+  createDescription () {
+    if (!this.description) return;
+    const desc = document.createElement('p');
+    desc.innerText = this.description;
+    
+    this.ele.appendChild(desc);
+  }
+    
+  createLink () {
+    const link = document.createElement('a');
+    link.href = this.url;
+    link.innerText = this.url;
+    
+    this.ele.appendChild(link);
+  }
+  
+}
